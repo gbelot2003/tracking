0f7eb51e1a63033a5efe9506041ebaf91b220c44
gbelot2003 <gbelot2003@hotmail.com>
2015-05-03 23:20:29 -0600

    Configurando elixir-gulp, bootstrap, materialize y js scripts para el front-end

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,16 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.less('app.less');
+    mix.sass('app.scss')
+});
+
+elixir(function(mix) {
+    mix.copy('bower_components/jquery/dist/jquery.min.js', 'resources/js/jquery.min.js');
+
+    mix.copy('bower_components/bootstrap-sass/assets/javascripts/bootstrap.min.js', 'resources/js/bootstrap.min.js');
+    mix.copy('bower_components/bootstrap-material-design/dist/js/material.min.js', 'resources/js/material.min.js');
+
+    mix.copy('bower_components/bootstrap-material-design/dist/js/ripples.min.js', 'resources/js/ripples.min.js');
+
+    mix.scripts(['jquery.min.js', 'bootstrap.min.js', 'material.min.js', 'ripples.min.js', 'myscripts.js'], 'public/js/app.js')
 });
