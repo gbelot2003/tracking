e708b16ed31b70d89391f2dcb89f2e2442fd5530
gbelot2003 <gbelot2003@hotmail.com>
2015-05-06 13:17:59 -0600

    agregando filtros de proteccion crfs a modelos de Permsission y Role

--- app/Permission.php
+++ app/Permission.php
@@ -4,5 +4,11 @@ use Zizaco\Entrust\EntrustPermission;
 
 class Permission extends EntrustPermission {
 
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = ['name', 'display_name', 'description'];
 
 }
