5086f2e6e23c93a145f8fcaf971d2e5e1bb45fee
gbelot2003 <gbelot2003@hotmail.com>
2015-05-16 17:25:12 -0600

    agregando atributos defauld a UserStatus

--- app/Userstatus.php
+++ app/Userstatus.php
@@ -4,6 +4,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Userstatus extends Model {
 
-	//
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'userstatuses';
 
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = ['name'];
 }
