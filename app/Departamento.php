e64ee001f642a8549a9c9a7055c09a707e45381f
gbelot2003 <gbelot2003@hotmail.com>
2015-05-17 20:51:22 -0600

    Agregando modelo de Departameto

---
+++ app/Departamento.php
@@ -0,0 +1,27 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Departamento extends Model {
+
+	/**
+	 * @var string
+	 */
+	protected $table = 'departamentos';
+
+	/**
+	 * @var bool
+	 */
+	public $timestamps = false;
+
+	/**
+	 * A Departamento tiene muchos Municipios
+	 *
+	 * @return \Illuminate\Database\Eloquent\Relations\HasMany
+	 */
+	public function municipios()
+	{
+		return $this->hasMany('App\Municipio');
+	}
+
+}
