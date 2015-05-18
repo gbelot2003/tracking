48aeaef2395c79047c9b61d72289d74311f0479f
gbelot2003 <gbelot2003@hotmail.com>
2015-05-17 20:52:12 -0600

    Agregando modelo de Seccion

---
+++ app/Seccion.php
@@ -0,0 +1,16 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Seccion extends Model {
+
+	/**
+	 * @var string
+	 */
+	protected $table = 'seccions';
+
+	/**
+	 * @var bool
+	 */
+	public $timestamps = false;
+}
