e2e918afd37731653c154135305f51ee8f2f820c
gbelot2003 <gbelot2003@hotmail.com>
2015-05-17 20:52:00 -0600

    Agregando modelo de Establecimiento

---
+++ app/Establecimiento.php
@@ -0,0 +1,17 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Establecimiento extends Model {
+
+	/**
+	 * @var string
+	 */
+	protected $table = 'establecimientos';
+
+	/**
+	 * @var bool
+	 */
+	public $timestamps = false;
+
+}
