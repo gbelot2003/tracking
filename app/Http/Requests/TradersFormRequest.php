bea5fea05e6ab529ccf7cbfd698e13749777d9f5
gbelot2003 <gbelot2003@hotmail.com>
2015-05-21 05:16:02 -0600

    TradersFormRequest actualizando las reglas de validacion acorde a unique_with

--- app/Http/Requests/TradersFormRequest.php
+++ app/Http/Requests/TradersFormRequest.php
@@ -21,16 +21,29 @@ class TradersFormRequest extends Request {
 	 */
 	public function rules()
 	{
-		$rules = [
+		$edit 	= [
 			'establecimiento_id'	=>	'required|Integer',
 			'seccion_id' 			=>	'required|Integer',
 			'cargo_id'				=>	'required|Integer',
-			'first_name'			=> 	'required|String',
+			'first_name'			=> 	"required|String",
 			'last_name'				=>  'required|String',
 			'user_id'				=> 	'integer'
 		];
 
-		return $rules;
+		$create = [
+			'establecimiento_id'	=>	'required|Integer',
+			'seccion_id' 			=>	'required|Integer',
+			'cargo_id'				=>	'required|Integer',
+			'first_name'			=> 	"required|String|unique_with:traders,first_name,last_name",
+			'last_name'				=>  'required|String',
+			'user_id'				=> 	'integer'
+		];
+
+		if($this->method == 'PUT'){
+			return $edit;
+		} else {
+			return $create;
+		}
 	}
 
 }
