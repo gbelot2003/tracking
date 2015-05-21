5cd58466729559ad0cb8a630f633f64b79a8b069
gbelot2003 <gbelot2003@hotmail.com>
2015-05-21 02:28:28 -0600

    TradeFormRequiest corrigiendo reglas de validacion para strings

--- app/Http/Requests/TradersFormRequest.php
+++ app/Http/Requests/TradersFormRequest.php
@@ -22,11 +22,11 @@ class TradersFormRequest extends Request {
 	public function rules()
 	{
 		$rules = [
-			'establecimiento_id'	=>	'required|integer',
-			'seccion_id' 			=>	'required|integer',
-			'cargo_id'				=>	'required|integer',
-			'first_name'			=> 	'String|integer',
-			'last_name'				=>  'String|integet',
+			'establecimiento_id'	=>	'required|Integer',
+			'seccion_id' 			=>	'required|Integer',
+			'cargo_id'				=>	'required|Integer',
+			'first_name'			=> 	'required|String',
+			'last_name'				=>  'required|String',
 			'user_id'				=> 	'integer'
 		];
 
