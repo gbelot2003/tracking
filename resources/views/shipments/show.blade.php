dd67d1c8f9c7ebdf5add1c2b60109e246170b015
gbelot2003 <gbelot2003@hotmail.com>
2015-05-22 02:25:52 -0600

    Shipments.show configurando salida de datos y creando partials _reviver y _sender para mantener organizada la vista

---
+++ resources/views/shipments/show.blade.php
@@ -0,0 +1,37 @@
+@extends('app')
+
+@section('title')
+	Ficha de encomienda No. {{ $paquete->id }}
+@stop
+
+
+@section('breadcrumbs')
+	<ol class="breadcrumb">
+		<li><a href="{{ url('home') }}">Inicio</a></li>
+		<li><a href="{{ url('shipments') }}">Listado de encomiendas</a></li>
+		<li><a href="{{ action('ShipmentCotroller@show', $paquete->id) }}">Encomienda No. {{ $paquete->id }}</a></li>
+	</ol>
+@stop
+
+@section('content')
+	@include('partials.flash')
+	@include('errors.form-error')
+
+	<div class="col-md-12">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="well">
+					<h4>Descripción</h4>
+					<p>{{ $paquete->description }}</p>
+				</div>
+			</div>
+			<div class="col-md-6">
+				@include('shipments._sender')
+			</div>
+
+			<div class="col-md-6">
+				@include('shipments._reciver')
+			</div>
+		</div>
+	</div>
+@stop
