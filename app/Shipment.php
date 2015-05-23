c0a2d20bdce5a1a59d1fdf99a271d35bbd1df0ae
gbelot2003 <gbelot2003@hotmail.com>
2015-05-22 23:48:57 -0600

    Shipment Model. agregando relacion con trancito

--- app/Shipment.php
+++ app/Shipment.php
@@ -15,14 +15,31 @@ class Shipment extends Model {
 	 */
 	protected $fillable = ['sender_id', 'reciber_id', 'description'];
 
+	/**
+	 * Un sender pertenece a un Trader
+	 * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+	 */
 	public function senders()
 	{
 		return $this->belongsTo('App\Trader', 'sender_id', 'id');
 	}
 
+	/**
+	 * Un reciver pretenece a un Trader
+	 * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+	 */
 	public function recivers()
 	{
 		return $this->belongsTo('App\Trader', 'reciber_id', 'id');
+	}
+
+	/**
+	 * Un encargo puede tener muchos trancitos
+	 * @return \Illuminate\Database\Eloquent\Relations\HasMany
+	 */
+	public function transitos()
+	{
+		return $this->hasMany('App\Trancito');
 	}
 
 }
