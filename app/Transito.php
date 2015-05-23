4eb6f283f51e25a277b89c087acf864cfbf73b79
gbelot2003 <gbelot2003@hotmail.com>
2015-05-22 22:54:20 -0600

    Transito Model, configuracion inicial de modelo

--- app/Transito.php
+++ app/Transito.php
@@ -4,6 +4,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transito extends Model {
 
-	//
+	/**
+	 *  The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'transitos';
+
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = ['shipment_id', 'estado_id', 'user_id', 'details'];
 
 }
