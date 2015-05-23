cbf943b5f7c9b74625977e76b8d20634f9fe69ef
gbelot2003 <gbelot2003@hotmail.com>
2015-05-22 23:41:57 -0600

    TransitoSeeder, creación y configuracion de archivo seeder

---
+++ database/seeds/TransitoTableSeeder.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ajax
+ */
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use App\Transito;
+
+class TransitoTableSeeder extends Seeder
+{
+    public function run()
+    {
+		DB::table('estados')->delete();
+
+		Transito::create([
+			'shipment_id' 	=> 1,
+			'estado_id' 	=> 1,
+			'details' 		=> 'Sin detalles'
+		]);
+
+		Transito::create([
+			'shipment_id' 	=> 2,
+			'estado_id' 	=> 1,
+			'details' 		=> 'Sin detalles'
+		]);
+
+		Transito::create([
+			'shipment_id' 	=> 3,
+			'estado_id' 	=> 1,
+			'details' 		=> 'Sin detalles'
+		]);
+
+		Transito::create([
+			'shipment_id' 	=> 4,
+			'estado_id' 	=> 1,
+			'details' 		=> 'Sin detalles'
+		]);
+
+		Transito::create([
+			'shipment_id' 	=> 5,
+			'estado_id' 	=> 1,
+			'details' 		=> 'Sin detalles'
+		]);
+
+		Transito::create([
+			'shipment_id' 	=> 6,
+			'estado_id' 	=> 1,
+			'details' 		=> 'Sin detalles'
+		]);
+
+		Transito::create([
+			'shipment_id' 	=> 7,
+			'estado_id' 	=> 1,
+			'details' 		=> 'Sin detalles'
+		]);
+
+		Transito::create([
+			'shipment_id' 	=> 8,
+			'estado_id' 	=> 1,
+			'details' 		=> 'Sin detalles'
+		]);
+
+	}
+}
