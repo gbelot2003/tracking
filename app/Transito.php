f488b801f991892352137907579ad534d8760d01
gbelot2003 <gbelot2003@hotmail.com>
2015-05-22 23:59:28 -0600

    Trancito Model. configurando relaciones

--- app/Transito.php
+++ app/Transito.php
@@ -18,4 +18,30 @@ class Transito extends Model {
 	 */
 	protected $fillable = ['shipment_id', 'estado_id', 'user_id', 'details'];
 
+	/**
+	 * Un trancito pretences a un Encargo
+	 * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+	 */
+	public function encargo()
+	{
+		return $this->belongsTo('App\Shipment');
+	}
+
+	/**
+	 * Un trancito pertenece a un usuario
+	 * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+	 */
+	public function user()
+	{
+		return $this->belongsTo('App\User');
+	}
+
+	/**
+	 * Un trancito pertenece un estado
+	 * @return \Illuminate\Database\Eloquent\Relations\belongsTo
+	 */
+	public function estados()
+	{
+		return $this->belongsTo('App\Estado');
+	}
 }
