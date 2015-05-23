9d5bbdd81449e4be0cded9d91968c2409b1a2f32
gbelot2003 <gbelot2003@hotmail.com>
2015-05-22 22:56:19 -0600

    Estado Model, configuracion inicial de modelo

--- app/Estado.php
+++ app/Estado.php
@@ -4,6 +4,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Estado extends Model {
 
-	//
+	/**
+	 *  The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'estados';
 
+	/**
+	 * @var bool
+	 */
+	public $timestamps = false;
 }
