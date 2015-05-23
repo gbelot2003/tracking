6e810432962c4e50a3337548ef6fac8976b6c773
gbelot2003 <gbelot2003@hotmail.com>
2015-05-22 21:39:37 -0600

    Shipments.edit, organizando elementos de la vista

--- resources/views/shipments/_form.blade.php
+++ resources/views/shipments/_form.blade.php
@@ -1,19 +1,34 @@
 <div class="row">
 	<div class="col-md-6">
-		<div class="col-md-12">
-		    <!-- sender Form Input -->
-			<div class="form-group">
-			    {!! Form::select('sender_id', $sender, null, ['class' => 'select form-control', 'id' => 'sender-select']) !!}
+		<div class="panel panel-info">
+			<div class="panel-heading">
+				<h3 class="panel-title">Destinatario</h3>
+			</div>
+			<div class="panel-body">
+				<!-- sender Form Input -->
+				<div class="form-group">
+					{!! Form::select('reciber_id', $reciver, null, ['class' => 'select form-control', 'id' => 'reciver-select']) !!}
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="col-md-6">
+		<div class="panel panel-primary">
+			<div class="panel-heading">
+				<h3 class="panel-title">Remitente</h3>
+			</div>
+			<div class="panel-body">
+				<!-- sender Form Input -->
+				<div class="form-group">
+					{!! Form::select('sender_id', $sender, null, ['class' => 'select form-control', 'id' => 'sender-select']) !!}
+				</div>
 			</div>
 		</div>
 
 	</div>
-	<div class="col-md-6">
-		<!-- sender Form Input -->
-		<div class="form-group">
-			{!! Form::select('reciber_id', $reciver, null, ['class' => 'select form-control', 'id' => 'reciver-select']) !!}
-		</div>
-	</div>
+
+
 	<div class="col-md-12">
 		<!-- Description Form Input -->
 		<div class="form-group">
