4c4ea408408c7b20c19fb9c185620904c2a154e2
gbelot2003 <gbelot2003@hotmail.com>
2015-05-24 17:13:18 -0600

    shipments.show agregando vista de query relacionadas de tracitos

--- resources/views/shipments/show.blade.php
+++ resources/views/shipments/show.blade.php
@@ -38,4 +38,26 @@
 
 		</div>
 	</div>
+	<div class="col-md-12">
+		<h3>Transitos</h3>
+		<table class="table table-hover table-border">
+			<caption>Estados de transito de esta encomienda</caption>
+			<thead>
+				<th>Estado</th>
+				<th>Usuario</th>
+				<th>Detalles</th>
+				<th>Hora</th>
+			</thead>
+			<tbody>
+				@foreach($paquete->transitos as $transito)
+				<tr>
+					<td>{{ $transito->estados->name }}</td>
+					<td>{{ $transito->user->name }}</td>
+					<td>{{ $transito->details }}</td>
+					<td>{{ $transito->created_at }}</td>
+				</tr>
+				@endforeach
+			</tbody>
+		</table>
+	</div>
 @stop
