b1ea017624fef5c46d78290d285fb9debdc3e436
gbelot2003 <gbelot2003@hotmail.com>
2015-05-25 13:13:08 -0600

    Trader vistas, varios cambios de organización logica

--- resources/views/trader/_informacion_individual.blade.php
+++ resources/views/trader/_informacion_individual.blade.php
@@ -1,28 +1,14 @@
 	<h3>Información General</h3>
-	<div class="row">
-		<div class="col-md-12 ">
-			<b>Nombre Completo</b>
-			<div class="warp shadow-z-2">
-				<div class="row">
-					<div class="col-md-12">
-						<h4 class="text-danger">{{ $trader->last_name }}, {{ $trader->first_name }}</h4>
-					</div>
-				</div>
-			</div>
+	<!-- Nav tabs -->
+	<ul class="nav nav-tabs" role="tablist">
+		<li role="presentation" class="active"><a href="#perfil" aria-controls="perfil" role="tab" data-toggle="tab">Datos del perfil</a></li>
+		<li role="presentation"><a href="#relacionados" aria-controls="relacionados" role="tab" data-toggle="tab">Usuarios relacionados</a></li>
+	</ul>
+	<div class="tab-content">
+		<div role="tabpanel" class="tab-pane active" id="perfil">
+			@include('trader._tabla_individual')
+		</div>
+		<div role="tabpanel" class="tab-pane" id="relacionados">
+			@include('trader._usuarios_relacionados')
 		</div>
 	</div>
-	<hr/>
-	<table class="table">
-		<thead>
-			<th>Edificio/Sucursal</th>
-			<th>Sección/Departamento</th>
-			<th>Cargo</th>
-		</thead>
-		<tbody>
-			<tr>
-				<td><h4>{{ $trader->establecimiento->name }}</h4></td>
-				<td><h4>{{ $trader->seccion->name }}</h4></td>
-				<td><h4>{{ $trader->cargo->name }}</h4></td>
-			</tr>
-		</tbody>
-	</table>
