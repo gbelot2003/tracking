11b09363c430fab3480aa985b6302ca1246c97c3
gbelot2003 <gbelot2003@hotmail.com>
2015-05-25 05:58:57 -0600

    error.form-error traduciendo mensaje de error

--- resources/views/errors/form-error.blade.php
+++ resources/views/errors/form-error.blade.php
@@ -1,6 +1,6 @@
 @if (count($errors) > 0)
 	<div class="alert alert-danger">
-		<strong>Whoops!</strong> There were some problems with your input.<br><br>
+		<strong>Whoops!</strong> Hay algunos problemas con su entrada de datos!!!.<br><br>
 		<ul>
 			@foreach ($errors->all() as $error)
 				<li>{{ $error }}</li>
