bc0a919e7c45a6e80ab91677501d6608bc591783
gbelot2003 <gbelot2003@hotmail.com>
2015-05-25 00:43:29 -0600

    user.show Vista, refactoring tipo de presentacion de la visata retoques

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -18,26 +18,29 @@
 @section('content')
 	@include('partials.flash')
 	<div class="row">
-		<div class="table-responsive">
-			<table class="table">
-				<thead>
-					<th>Apellido(s):</th>
-					<th>Nombre(s):</th>
-					<th>Email:</th>
-					<th>Rol:</th>
-				</thead>
-				<tbody>
-					<tr>
-						<td>{{ $user->last_name }}</td>
-						<td>{{ $user->first_name }}</td>
-						<td>{{ $user->email }}</td>
-						<td>
-							@foreach($user->roles as $rol)
-								{{ $rol->display_name }}
-							@endforeach
-						</td>
-					</tr>
-				</tbody>
-			</table>
+		<div class="col-md-12">
+			<div class="table-responsive">
+				<table class="table">
+					<thead>
+						<th>Apellido(s):</th>
+						<th>Nombre(s):</th>
+						<th>Email:</th>
+						<th>Rol:</th>
+					</thead>
+					<tbody>
+						<tr>
+							<td>{{ $user->last_name }}</td>
+							<td>{{ $user->first_name }}</td>
+							<td>{{ $user->email }}</td>
+							<td>
+								@foreach($user->roles as $rol)
+									{{ $rol->display_name }}
+								@endforeach
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
 		</div>
+	</div>
 @stop
