b0307928573349afc0381c673ce166911933acd8
gbelot2003 <gbelot2003@hotmail.com>
2015-05-25 13:14:14 -0600

    paquetes.paquetes vista, configurando tablas landing page

--- resources/views/paquetes/paquetes.blade.php
+++ resources/views/paquetes/paquetes.blade.php
@@ -11,3 +11,25 @@
 		<li><a href="{{ url('mis-paquetes') }}">Administración de paquetes</a></li>
 	</ol>
 @stop
+
+@section('content')
+	<table class="table table-hover table-border">
+		<caption>Listado de mis usuarios relacionados</caption>
+		<thead>
+			<th>Nombre</th>
+			<th>Establecimiento</th>
+			<th>Sección</th>
+			<th>Cargo</th>
+		</thead>
+		<tbody>
+			@foreach($clientes as $cliente)
+				<tr>
+					<td><a href="{{ action('TraderController@show', $cliente->id)  }}">{{ $cliente->first_name }} {{ $cliente->last_name }}</a></td>
+					<td>{{ $cliente->establecimiento->name}}</td>
+					<td>{{ $cliente->seccion->name}}</td>
+					<td>{{ $cliente->cargo->name}}</td>
+				</tr>
+			@endforeach
+		</tbody>
+	</table>
+@stop
