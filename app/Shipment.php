2ae4dbe0927a9f2ffd502a3104b01b9adf819f94
gbelot2003 <gbelot2003@hotmail.com>
2015-05-25 05:03:51 -0600

    Shipment Modelo, agregando code a $fillable

--- app/Shipment.php
+++ app/Shipment.php
@@ -13,7 +13,7 @@ class Shipment extends Model {
 	 * Proteccion para MassAssigments
 	 * @var array
 	 */
-	protected $fillable = ['sender_id', 'reciber_id', 'description'];
+	protected $fillable = ['code', 'sender_id', 'reciber_id', 'description'];
 
 	/**
 	 * Un sender pertenece a un Trader
