8093158674c5291a1520a8b4e49f551fd7d435cc
gbelot2003 <gbelot2003@hotmail.com>
2015-05-24 19:41:23 -0600

    trader._ultimas_encomiendas vistas, Agregando funcionalidad de tabs y partials de tablas relacionadas

--- resources/views/trader/_ultimas_encomiendas.blade.php
+++ resources/views/trader/_ultimas_encomiendas.blade.php
@@ -1,4 +1,16 @@
-<div class="col-md-5">
 	<h3>Ultimas Encomiendas</h3>
-	<div class="row"></div>
-</div>
+	<!-- Nav tabs -->
+	<ul class="nav nav-tabs" role="tablist">
+		<li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Recividas/Esperando</a></li>
+		<li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Envios</a></li>
+	</ul>
+	<div class="tab-content">
+		<div role="tabpanel" class="tab-pane active" id="home">
+			<h4>Recividas/Esperando</h4>
+			@include('trader._reciver_table')
+		</div>
+		<div role="tabpanel" class="tab-pane" id="profile">
+			<h4>Envios</h4>
+			@include('trader._sender_table')
+		</div>
+	</div>
