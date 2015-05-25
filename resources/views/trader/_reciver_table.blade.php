2120ce06859dafa1493f52200366d3f63d489277
gbelot2003 <gbelot2003@hotmail.com>
2015-05-24 19:40:02 -0600

    trader. vistas, organizando informacion en bloques para tablas de reciver y sender

---
+++ resources/views/trader/_reciver_table.blade.php
@@ -0,0 +1,26 @@
+<table class="table">
+	<thead>
+	<th>No.</th>
+	<th>Remitente</th>
+	<th>Fecha</th>
+	<th>Estado</th>
+	</thead>
+	<tbody>
+	@foreach($reciber as $paquete)
+		<tr>
+			<td><b>{{ $paquete->id }}</b></td>
+			<td>
+				<div class="info-row">{{ $paquete->senders->first_name }}, {{ $paquete->senders->last_name }}</div>
+				<div class="info-row"><span class="info-row">{{ $paquete->senders->cargo->name }}</span></div>
+				<div class="info-row"><span class="info-row">{{ $paquete->senders->seccion->name }}</span></div>
+			</td>
+			<td>{{ $paquete->created_at }}</td>
+			<td>
+				@foreach($paquete->transitos->slice(0, 1) as $transito)
+					{{ $transito->estados->name }}
+				@endforeach
+			</td>
+		</tr>
+	@endforeach
+	</tbody>
+</table>
