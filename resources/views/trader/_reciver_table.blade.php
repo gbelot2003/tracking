dd267640bab9e35c4add7f9c1f96ede0dbdda753
gbelot2003 <gbelot2003@hotmail.com>
2015-05-25 14:05:33 -0600

    trader.reciver

--- resources/views/trader/_reciver_table.blade.php
+++ resources/views/trader/_reciver_table.blade.php
@@ -8,7 +8,7 @@
 	<tbody>
 	@foreach($reciber as $paquete)
 		<tr>
-			<td><b>{{ $paquete->id }}</b></td>
+			<td><b>{{ $paquete->code }}</b></td>
 			<td>
 				<div class="info-row">{{ $paquete->senders->first_name }}, {{ $paquete->senders->last_name }}</div>
 				<div class="info-row"><span class="info-row">{{ $paquete->senders->cargo->name }}</span></div>
