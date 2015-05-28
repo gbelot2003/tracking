8472ccddfb9703b48a08c9fd7d125c052332f124
gbelot2003 <gbelot2003@hotmail.com>
2015-05-28 09:55:07 -0600

    forms.js

--- public/js/forms.js
+++ public/js/forms.js
@@ -1,22 +1,25 @@
-jQuery(document).ready(function(){
-    /** Shipments.show form script **/
-    $('form[data-ask-submit]').on('click', function(e){
-        e.preventDefault();
-        bootbox.dialog({
-            message: "Estas seguro de enviar este transito correcto??",
-            title: "Transito Correcto",
-            buttons: {
-                success: {
-                    label: "Enviar!",
-                    className: "btn-success",
-                    callback: function() {
-                        $('form[data-ask-submit]').submit();
+(function(){
+    jQuery(document).ready(function(){
+        /** Shipments.show form script **/
+        $('form[data-ask-submit]').on('click', function(e){
+            e.preventDefault();
+            bootbox.dialog({
+                message: "Estas seguro de enviar este transito correcto??",
+                title: "Transito Correcto",
+                buttons: {
+                    success: {
+                        label: "Enviar!",
+                        className: "btn-success",
+                        callback: function() {
+                            $('form[data-ask-submit]').submit();
+                        }
                     }
                 }
-            }
+            });
         });
+
+
     });
+})();
 
-
-});
 //# sourceMappingURL=forms.js.map
