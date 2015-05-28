62aa8ce2d5f43426df1348f449b1151e38614406
gbelot2003 <gbelot2003@hotmail.com>
2015-05-28 10:47:40 -0600

    shipments vistas, reacomodando markup

--- resources/views/shipments/_reciver.blade.php
+++ resources/views/shipments/_reciver.blade.php
@@ -6,24 +6,22 @@
 		<table class="table table-bordered">
 			<thead>
 			<th>Nombre</th>
-			<th>Cargo</th>
 			<th>Sección</th>
+			<th>Sucursal/Edificio</th>
 			</thead>
 			<tbody>
 			<tr>
 				<td><span class="info-row">{{ $paquete->recivers->first_name }} {{ $paquete->senders->last_name }}</span></td>
-				<td><span class="info-row"><span class="info-row">{{ $paquete->recivers->cargo->name }}</span></span></td>
 				<td><span class="info-row"><span class="info-row">{{ $paquete->recivers->seccion->name }}</span></span></td>
-			</tr>
-			<tr>
-				<th>Sucursal/Edificio</th>
-				<th>Departamento</th>
-				<th>Municipio</th>
-			</tr>
-			<tr>
 				<td>{{ $paquete->recivers->establecimiento->name }}</td>
+			</tr>
+			<tr>
+				<th>Departamento</th>
+				<th colspan="2">Municipio</th>
+			</tr>
+			<tr>
 				<td>{{ $paquete->recivers->establecimiento->departamento->departamento }}</td>
-				<td>{{ $paquete->recivers->establecimiento->municipio->municipio }}</td>
+				<td colspan="2">{{ $paquete->recivers->establecimiento->municipio->municipio }}</td>
 			</tr>
 			<tr>
 				<th colspan="3">Dirección</th>
--- resources/views/shipments/_sender.blade.php
+++ resources/views/shipments/_sender.blade.php
@@ -6,24 +6,24 @@
 		<table class="table table-bordered">
 			<thead>
 			<th>Nombre</th>
-			<th>Cargo</th>
 			<th>Sección</th>
+			<th>Sucursal/Edificio</th>
 			</thead>
 			<tbody>
 			<tr>
 				<td><span class="info-row">{{ $paquete->senders->first_name }} {{ $paquete->senders->last_name }}</span></td>
-				<td><span class="info-row"><span class="info-row">{{ $paquete->senders->cargo->name }}</span></span></td>
 				<td><span class="info-row"><span class="info-row">{{ $paquete->senders->seccion->name }}</span></span></td>
-			</tr>
-			<tr>
-				<th>Sucursal/Edificio</th>
-				<th>Departamento</th>
-				<th>Municipio</th>
-			</tr>
-			<tr>
 				<td>{{ $paquete->senders->establecimiento->name }}</td>
+
+			</tr>
+			<tr>
+
+				<th>Departamento</th>
+				<th colspan="2">Municipio</th>
+			</tr>
+			<tr>
 				<td>{{ $paquete->senders->establecimiento->departamento->departamento }}</td>
-				<td>{{ $paquete->senders->establecimiento->municipio->municipio }}</td>
+				<td colspan="2">{{ $paquete->senders->establecimiento->municipio->municipio }}</td>
 			</tr>
 			<tr>
 				<th colspan="3">Dirección</th>
