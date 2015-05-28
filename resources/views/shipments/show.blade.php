aae5bbc5a94d9add209818a0ca9d5aa6527c1be8
gbelot2003 <gbelot2003@hotmail.com>
2015-05-28 07:58:36 -0600

    shipment.show incluyendo nueva vista

--- resources/views/shipments/show.blade.php
+++ resources/views/shipments/show.blade.php
@@ -41,42 +41,6 @@
 		</div>
 	</div>
 	<div class="col-md-12">
-		<div class="row">
-			<div class="col-md-3">
-				<h3>Transitos</h3>
-			</div>
-			<div class="col-md-2">
-				{!! Form::open(['action' => ['TransitosController@store'], 'class' => 'form-inline']) !!}
-					{!! Form::submit('Transito', ['class' => 'btn btn-primary']) !!}
-					{!! Form::hidden('estado_id', 2) !!}
-					{!! Form::hidden('shipment_id', $paquete->id) !!}
-					{!! Form::hidden('details', "Todo Correcto") !!}
-				{!! Form::close() !!}
-			</div>
-			<div class="col-md-3">
-				<a id="virtual-form" class="btn btn-material-cyan" href="{{ action('TransitosController@create', $paquete->id) }}">Transito observaciones</a>
-			</div>
-
-		</div>
-
-		<table class="table table-hover table-border">
-			<caption>Estados de transito de esta encomienda</caption>
-			<thead>
-				<th>Estado</th>
-				<th>Usuario</th>
-				<th>Detalles</th>
-				<th>Hora</th>
-			</thead>
-			<tbody>
-				@foreach($paquete->transitos as $transito)
-				<tr>
-					<td>{{ $transito->estados->name }}</td>
-					<td>{{ $transito->user->name }}</td>
-					<td>{{ $transito->details }}</td>
-					<td>{{ $transito->created_at }}</td>
-				</tr>
-				@endforeach
-			</tbody>
-		</table>
+		@include('shipments._rel_transitos')
 	</div>
 @stop
