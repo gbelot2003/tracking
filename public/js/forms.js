76e180b8c72d9ada6314927adc6f5467a0fca5e2
gbelot2003 <gbelot2003@hotmail.com>
2015-05-29 02:19:31 -0600

    form.js agregando metodos de ajaxStar(incompleto)

--- public/js/forms.js
+++ public/js/forms.js
@@ -1,5 +1,26 @@
 (function(){
+    $.ajaxSetup({global:true});
+    $(document).ajaxStart(function(e) {
+        $("#loading").show();
+    });
+
+    $(document).ajaxStop(function(e) {
+        $("#loading").hide();
+    });
     jQuery(document).ready(function(){
+        $('#sender-select').select2({ placeholder: "Select a state" });
+        $('#reciver-select').select2({ placeholder: "Select a state" });
+        $('#establecimiento-select').select2({ placeholder: 'Escoge una Sucursal' });
+        $('#secciones-select').select2({ placeholder: 'Escoge una Sección' });
+        $('#cargo-select').select2({ placeholder: 'Escoge una Sección' });
+        $('#dept-select').select2();
+        /** establecimientos.create/edit form script **/
+        var mid = $("#dept-select option:selected").val();
+        $('#municipios').load("/querys/municipios/" + mid, function(){ $('#munic-select').select2(); });
+        $('#dept-select').on('change', function(){
+            var mid = $("#dept-select option:selected").val();
+                $('#municipios').load("/querys/municipios/" + mid, function(){ $('#munic-select').select2(); });
+            });
         /** Shipments.show form script **/
         $('form[data-ask-submit]').on('click', function(e){
             e.preventDefault();
@@ -17,22 +38,25 @@
                 }
             });
         });
-        /** establecimientos.create/edit form script **/
-        jQuery(document).ready(function(){
-            var mid = $("#dept-select option:selected").val();
-            $('#municipios').load("/querys/municipios/" + mid, function(){
-                $('#munic-select').select2();
-            });
-
-            $('#dept-select').on('change', function(){
-                var mid = $("#dept-select option:selected").val();
-                $('#municipios').load("/querys/municipios/" + mid, function(){
-                    $('#munic-select').select2();
+        /***otro script*/
+        $('#cerrar-perfil').hide();
+        $('#perfil').on('click', function(e){
+            e.preventDefault();
+            $(this).hide();
+            $('#cerrar-perfil').show(function(){
+                $(this).on('click', function(e){
+                    e.preventDefault();
+                    $('#cerrar-perfil').hide();
+                    $('#create-form').slideUp();
+                    $('#perfil').show('slow');
                 });
             });
+            $('#create-form').load('/personal/create', function(){
+                $(this).slideDown().css({'background' : '#EEEEEE', 'margin-bottom':'3rem'});
+                $('#establecimiento-select').select2({ placeholder: 'Escoge una Sucursal' });
+                $('#secciones-select').select2({ placeholder: 'Escoge una Sección' });
+            });
         });
-
-        $('#dept-select').select2();
     });
 })();
 
