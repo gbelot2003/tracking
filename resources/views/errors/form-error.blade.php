c826c8048bd1a20cdcfb5a6e22aea5db4c8eda2b
gbelot2003 <gbelot2003@hotmail.com>
2015-05-28 20:03:53 -0600

    form-error corrigiendo color y estandarizando markup

--- resources/views/errors/form-error.blade.php
+++ resources/views/errors/form-error.blade.php
@@ -1,5 +1,6 @@
 @if (count($errors) > 0)
-	<div class="alert alert-danger">
+	<div class="alert alert-danger" style="color:white">
+		<button class="close" type="button" data-dismiss="alert" aria-hidden="true">&times;</button>
 		<strong>Whoops!</strong> Hay algunos problemas con su entrada de datos!!!.<br><br>
 		<ul>
 			@foreach ($errors->all() as $error)
@@ -8,3 +9,6 @@
 		</ul>
 	</div>
 @endif
+
+
+
