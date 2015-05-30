6153ba66f60af755b5e924ea156c37deb36ff3b2
gbelot2003 <gbelot2003@hotmail.com>
2015-05-30 03:35:20 -0600

    User Migracion actualizada

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,8 +19,7 @@ class CreateUsersTable extends Migration {
 			$table->string('name');
 			$table->string('first_name')->nullable();
 			$table->string('last_name')->nullable();
-			$table->integer('area_id')->unsigned();
-			$table->integer('cargo_id')->unsigned();
+			$table->integer('area_id')->unsigned(); // area geografica
 			$table->string('email')->unique();
 			$table->string('password', 60);
 			$table->rememberToken();
