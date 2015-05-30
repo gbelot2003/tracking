f32e0b0434623e42be0d63325fc36d6ad4709b78
gbelot2003 <gbelot2003@hotmail.com>
2015-05-30 03:23:08 -0600

    Establecimientos Migration Update

--- database/migrations/2015_05_17_223734_create_establecimientos_table.php
+++ database/migrations/2015_05_17_223734_create_establecimientos_table.php
@@ -12,18 +12,23 @@ class CreateEstablecimientosTable extends Migration {
 	 */
 	public function up()
 	{
+		Schema::create('testablecimientos', function(Blueprint $table){
+			$table->increments('id');
+			$table->string('name');
+		});
+
 		Schema::create('establecimientos', function(Blueprint $table)
 		{
 			$table->increments('id');
+			$table->integer('testablecimiento_id')->unsigned();
 			$table->integer('departamento_id')->unsigned();
 			$table->integer('municipio_id')->unsigned();
 			$table->string('name');
 			$table->text('address')->nullable();
-		});
-
-		Schema::create('testablecimientos', function(Blueprint $table){
-			$table->increments('id');
-			$table->string('name');
+			$table->foreign('testablecimiento_id')
+				->references('id')
+				->on('testablecimientos')
+				->onDelete('cascade');
 		});
 	}
 
@@ -35,6 +40,7 @@ class CreateEstablecimientosTable extends Migration {
 	public function down()
 	{
 		Schema::drop('establecimientos');
+		Schema::drop('testablecimientos');
 	}
 
 }
