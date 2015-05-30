234559939a2355ce786da8eb558d16e834782083
gbelot2003 <gbelot2003@hotmail.com>
2015-05-30 02:01:19 -0600

    Area Model, agregando metodos genericos

--- app/Area.php
+++ app/Area.php
@@ -4,6 +4,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Area extends Model {
 
-	//
+	/**
+	 * @var string
+	 */
+	protected $table = 'Areas';
+
+	/**
+	 * @var bool
+	 */
+	public $timestamps = false;
 
 }
