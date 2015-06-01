8e19c392bca758673d40cd659294b88c98b529d8
gbelot2003 <gbelot2003@hotmail.com>
2015-06-01 04:52:24 -0600

    Transitos migración agregando campos para ruta de imagenes

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,8 @@ elixir(function(mix) {
     mix.copy('bower_components/select2-dist/dist/css/select2.min.css', 'resources/assets/sass/select2.min.scss');
     mix.copy('bower_components/bootstrap-material-design/dist/js/material.min.js', 'resources/assets/js/material.min.js');
     mix.copy('bower_components/bootstrap-material-design/dist/js/ripples.min.js', 'resources/assets/js/ripples.min.js');
-    mix.scripts(['jquery-1.11.3.min.js', 'bootstrap.min.js', 'arrive.js', 'material.min.js', 'ripples.min.js', 'bootbox.min.js', 'select2.full.min.js', 'init.js', 'flexigrid.js' , 'jquery.dataTables.js'], 'public/js/app.js')
+    mix.scripts(['jquery-1.11.3.min.js', 'bootstrap.min.js', 'arrive.js', 'material.min.js', 'ripples.min.js', 'bootbox.min.js',
+                 'select2.full.min.js', 'flexigrid.js' , 'jquery.dataTables.js', 'init.js', 'myscripts.js'], 'public/js/app.js')
     mix.scripts(['forms.js'], 'public/js/forms.js');
 
 
