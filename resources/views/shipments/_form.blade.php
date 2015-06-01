b47560e8c61689017add5bc0bade4af7a3387adc
Gerardo Belot <gerardobelot@outlook.com>
2015-06-01 15:21:38 -0600

    Update _form.blade.php

--- resources/views/shipments/_form.blade.php
+++ resources/views/shipments/_form.blade.php
@@ -1,8 +1,8 @@
 <div class="row">
-	<div class="col-md-6">
+	<div class="col-md-12">
 		<div class="panel panel-primary">
 			<div class="panel-heading">
-				<h3 class="panel-title">Destinatario</h3>
+				<h3 class="panel-title">codigo</h3>
 			</div>
 			<div class="panel-body">
 			
