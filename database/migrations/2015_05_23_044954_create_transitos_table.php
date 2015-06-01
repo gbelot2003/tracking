45b77d181378d78ee57e539ae101ec02cc005d30
gbelot2003 <gbelot2003@hotmail.com>
2015-06-01 02:48:32 -0600

    Transitos migración agregando campos para ruta de imagenes

--- database/migrations/2015_05_23_044954_create_transitos_table.php
+++ database/migrations/2015_05_23_044954_create_transitos_table.php
@@ -20,6 +20,8 @@ class CreateTransitosTable extends Migration {
 			$table->integer('estado_id')->unsigned();
 			$table->integer('user_id')->unsigned();
 			$table->text('details')->nullable();
+			$table->string('sing_path')->nullable();
+			$table->string('picture_path')->nullable();
 			$table->timestamps();
 			$table->foreign('shipment_id')
 				->references('id')
