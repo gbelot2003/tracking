7499974b8d0b928902e57668239a5a9fa345c458
gbelot2003 <gbelot2003@hotmail.com>
2015-06-11 00:29:35 -0600

    BolsasFormRequest

--- app/Http/Requests/BolsasFormRequest.php
+++ app/Http/Requests/BolsasFormRequest.php
@@ -21,29 +21,43 @@ class BolsasFormRequest extends Request {
 	 */
 	public function rules()
 	{
+		$shipment = count($this->request->get('shipment_id'));
 		$create = [
-			'code' => "integer|required",
+			'code' => "required|integer|unique:bolsas",
 			'envios_establecimiento_id' => 'interger',
+			'destino_id' => 'integer',
 			'establecimiento_id' => 'integer'
 		];
 
 		$edit = [
-			'code' => "integer|required",
+			'code' => "required|integer",
 			'envios_establecimiento_id' => 'interger',
+			'destino_id' => 'integer',
 			'establecimiento_id' => 'integer'
 		];
 
 		if($this->method == 'PUT'){
-			foreach($this->request->get('shipment_id') as $key => $val)
+			if($shipment > 1)
 			{
-				$create['shipment_id.'.$key] = 'required|max:10';
+				foreach($this->request->get('shipment_id') as $key => $val)
+				{
+					$create['shipment_id.'.$key] = 'required|max:10';
+				}
+			} else {
+				$create = ['shipment_id' => 'required'];
 			}
 
 			return $create;
 		} else {
-			foreach($this->request->get('shipment_id') as $key => $val)
+
+			if($shipment > 1)
 			{
-				$edit['shipment_id.'.$key] = 'required|max:10';
+				foreach($this->request->get('shipment_id') as $key => $val)
+				{
+					$edit['shipment_id.'.$key] = 'required|max:10';
+				}
+			}else {
+				$edit = ['shipment_id' => 'required'];
 			}
 			return $edit;
 		}
