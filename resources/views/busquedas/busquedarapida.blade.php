de95e732485e93eeaad75b6fb6407f34d4664b50
gbelot2003 <gbelot2003@hotmail.com>
2015-06-17 16:11:07 -0600

    Busqueda Rapida, estructuración de busquedas

--- resources/views/busquedas/busquedarapida.blade.php
+++ resources/views/busquedas/busquedarapida.blade.php
@@ -1,1 +1,6 @@
+@extends('app')
+@section('content')
 <h3>Sin Resultados</h3>
+<p>La busqueda no a encontrado resultados, verifica el numero o usa el scaner, si aun así no hay resultados, ponte en contacto con tu supervisos o los administradores
+de la aplicación.</p>
+@stop
