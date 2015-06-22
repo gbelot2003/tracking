28a41201ed6a71225f9ee8be479a42bcd5e116d5
gbelot2003 <gbelot2003@hotmail.com>
2015-06-22 01:15:26 -0600

    reportes.usuarios.usuarios, creando listado para reportes

--- resources/views/reportes/usuarios/usuarios.blade.php
+++ resources/views/reportes/usuarios/usuarios.blade.php
@@ -9,3 +9,52 @@
 	</ol>
 @stop
 
+@section('content')
+	<table id="users-table" class="table table-hover table-borderd">
+		<thead>
+			<th>No.</th>
+			<th>Usuario</th>
+			<th>email</th>
+			<th>Cargo</th>
+			<th>Ubicación</th>
+		</thead>
+		<tbody>
+		@foreach($user as $user)
+			<tr>
+				<td>{{ $user->id }}</td>
+				<td>{{ $user->name }}</td>
+				<td>{{ $user->email }}</td>
+				<td>
+					@foreach($user->roles as $role)
+						{{ $role->name }}
+					@endforeach
+				</td>
+				<td>{{ $user->establecimiento->name }}</td>
+			</tr>
+		@endforeach
+		</tbody>
+	</table>
+@stop
+
+@section('post-script')
+	<script>
+		$(function() {
+			$('#users-table').DataTable({
+				"language": {
+					"lengthMenu": "Mostrar _MENU_ registros por página",
+					"zeroRecords": "Registro no encotrado - lo sentimos",
+					"info": "Mostrando página _PAGE_ de _PAGES_",
+					"infoEmpty": "No hay registros de esa busqueda",
+					"infoFiltered": "(filtrado de _MAX_ total Total de regístros)",
+					"search":  "Busqueda:",
+					"paginate": {
+						"first":      "Primero",
+						"last":       "Ultimo",
+						"next":       "Siguiente",
+						"previous":   "Anterior"
+					}
+				}
+			});
+		});
+	</script>
+@stop
