f1935070a32a372fca8c13fb6e0941edec313487
gbelot2003 <gbelot2003@hotmail.com>
2015-06-23 19:33:43 -0600

    TransitosController, Delegando funciones de creación de registros a SaveTransitos Listener

--- app/Http/Controllers/TransitosController.php
+++ app/Http/Controllers/TransitosController.php
@@ -1,5 +1,6 @@
 <?php namespace App\Http\Controllers;
 
+use App\Event\SaveTransitos;
 use App\Http\Requests;
 use App\Http\Requests\TransitosFormRequest;
 use App\Shipment;
@@ -45,92 +46,7 @@ class TransitosController extends Controller {
 	 */
 	public function store(TransitosFormRequest $request)
 	{
-
-		$firma_name = null;
-		$foto_name = null;
-		$establecimiento = Auth::user()->establecimiento_id;
-		$cerrar_trancito = $request->input('estado_id');
-		$shipmente = Shipment::findOrFail($request->input('shipment_id'));
-		$cerrada = $shipmente->estado_id;
-
-		DB::transaction(function() use ($request, $establecimiento, $cerrar_trancito, $shipmente, $cerrada, &$data){
-
-			if ($cerrada == 8 or $cerrada == 9 or $cerrada == 10 or $cerrada == 11 or $cerrada == 12 or $cerrada == 13)
-			{
-				return redirect()->back()->with('errors', 'Esta encomienda ya a sido entregada cerrada');
-			}
-
-			$date = date('Y-m-d h:m');
-
-			if($request->hasFile('firma'))
-			{
-				$firma = $request->input('shipment_id') . '.' . $request->file('firma')->getClientOriginalExtension();
-				$firma_name = "firma-" . $date .'--'. $firma;
-				$request->file('firma')->move(base_path() . '/public/images/transitos/firmas/', $firma_name);
-			} else {
-				$firma_name = null;
-			}
-
-			if($request->hasFile('foto'))
-			{
-				$foto = $request->input('shipment_id') . '.' . $request->file('foto')->getClientOriginalExtension();
-				$foto_name = "foto-" . $date .'-'. $foto;
-				$request->file('foto')->move(base_path() . '/public/images/transitos/fotos/', $foto_name);
-			} else{
-				$foto_name = null;
-			}
-
-			$transitos = Transito::create([
-				'shipment_id' => $request->input('shipment_id'),
-				'estado_id' => $request->input('estado_id'),
-				'establecimiento_id' => $establecimiento,
-				'user_id' => Auth::id(),
-				'details' => $request->input('details'),
-				'firma' => $firma_name,
-				'foto' => $foto_name
-			]);
-
-			$shipmente->estado_id = $request->input('estado_id');
-			$shipmente->user_id = Auth::id();
-			$shipmente->transito_id = $transitos->id;
-			$shipmente->firma = $firma_name;
-			$shipmente->save();
-
-		});
-
-		/**
-		 * estado -> 1 abierto a trancitos
-		 * estado -> 2 en centro de acopio / abierto a transitos
-		 * estado -> 3 el paquete esta cerrado para mas transitos
-		 * reflex shipments._rel_tranbsitos
-		 */
-		switch ($cerrar_trancito) {
-			case 3:
-				$message = "Registro ingresado en centro de acopio";
-				break;
-			case 8:
-				$message = "Registro editado, y entrega cerrada";
-				break;
-			case 9:
-				$message = "Registro editado, y entrega cerrada";
-				break;
-			case 10:
-				$message = "Registro editado, y entrega cerrada";
-				break;
-			case 11:
-				$message = "Registro editado, y entrega cerrada";
-				break;
-			case 12:
-				$message = "Registro editado, y entrega cerrada";
-				break;
-			case 13:
-				$message = "Registro editado, y entrega cerrada";
-				break;
-			default:
-				$message = 'Registro editado';
-		}
-
-		Session::flash('flash_message', $message);
+		event(new SaveTransitos($request->all()));
 		return redirect()->route('shipments.show',$request->input('shipment_id'));
 	}
 
