fd6f38c41c57f214d6bdd6c0691c57c9993252ec
gbelot2003 <gbelot2003@hotmail.com>
2015-06-23 20:27:33 -0600

    SaveBolsas Event and listeners creados

---
+++ app/Event/SaveBolsas.php
@@ -0,0 +1,25 @@
+<?php namespace App\Event;
+
+use App\Events\Event;
+
+use App\Shipment;
+use Illuminate\Queue\SerializesModels;
+
+class SaveBolsas extends Event {
+
+	use SerializesModels;
+	public $request;
+	public $shipments_id;
+	public $count;
+	public $bolsas;
+	public $transitoBolsas;
+	public $transito;
+
+	public function __construct($request)
+	{
+		$this->request = $request;
+		$this->shipments_id = $request['shipment_id'];
+		$this->count = count($this->shipments_id);
+	}
+
+}
