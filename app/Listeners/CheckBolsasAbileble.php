b29ad7e45b1c8531a1c82a598ab2cdbb51da6bc3
gbelot2003 <gbelot2003@hotmail.com>
2015-06-27 01:59:51 -0600

    CheckBolsasAbileble Agregando condicional para cierre de proceso y mensaje

--- app/Listeners/CheckBolsasAbileble.php
+++ app/Listeners/CheckBolsasAbileble.php
@@ -11,16 +11,21 @@ class CheckBolsasAbileble {
 
 	public function handle(SaveBolsas $event)
 	{
-		for($this->i = 0; $this->i < $event->count; $this->i++){
+		for ($this->i = 0; $this->i < $event->count; $this->i++)
+		{
 
 			$event->shipments = Shipment::findOrFail($event->shipments_id[$this->i]);
 
-			if(count($event->shipments->bolsas) > 0){
-				Session::flash('flash_message', "El registro con numero " . $event->shipments->code . " se encuentra registrada en otra bolsa, La bolsa no a sido creada");
-				Session::flash('flash_message_important', true);
-				return false;
+			if (count($event->shipments->bolsas) > 0)
+			{
+					$cerrados = [8, 9, 10, 11, 12, 13];
+					if (in_array($cerrados, $event->shipments->estado_id)){
+						Session::flash('flash_message', "El registro con numero " . $event->shipments->code . " se encuentra registrada en otra bolsa, La bolsa no a sido creada");
+						Session::flash('flash_message_important', true);
+
+					return false;
+				}
 			}
 		}
 	}
-
 }
