a464f8c8d2b02e9fa5790491a701174e6a451439
gbelot2003 <gbelot2003@hotmail.com>
2015-07-10 16:21:03 -0600

    correcciones

--- resources/views/trader/edit.blade.php
+++ resources/views/trader/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('app')
 
 @section('title')
-	Editando registro de {{ $trader->last_name }}, {{ $trader->first_name }}
+	Editando registro de {{ $trader->name }}
 @stop
 
 
@@ -10,7 +10,7 @@
 		<li><a href="{{ url('home') }}">Inicio</a></li>
 		<li><a href="{{ url('personal') }}">Administración de Personal</a></li>
 		<li><a href="{{ action('TraderController@show', $trader->id) }}">{{ $trader->name }}</a></li>
-		<li><a href="#!">Edición {{ $trader->last_name }}, {{ $trader->first_name }} </a></li>
+		<li><a href="#!">Edición {{ $trader->name }}</a></li>
 	</ol>
 @stop
 
