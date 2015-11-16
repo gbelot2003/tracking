3a94f30686cf46cdb25aff3801c523eb4e2fc80e
gbelot2003 <gbelot2003@hotmail.com>
2015-11-16 08:04:25 -0600

    Actualizando metodo de creación de Bolsa

--- app/Http/Controllers/ApiBolsasController.php
+++ app/Http/Controllers/ApiBolsasController.php
@@ -4,7 +4,11 @@ use App\Bolsa;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
+use App\Http\Requests\BolsasFormRequest;
+use App\TransitoBolsa;
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ApiBolsasController extends Controller {
 
@@ -36,11 +40,22 @@ class ApiBolsasController extends Controller {
 	/**
 	 * Store a newly created resource in storage.
 	 *
+	 * @param BolsasFormRequest $request
 	 * @return Response
 	 */
-	public function store()
+	public function store(BolsasFormRequest $request)
 	{
-		//
+		//Pendiente arreglar transacción
+		$prebolsa = new Bolsa($request->all());
+		$bolsa = Auth::user()->bolsas()->save($prebolsa);
+		$transito = new TransitoBolsa([
+			'bolsa_id' 				=> $bolsa->id,
+			'estado_id' 			=> $bolsa->estado_id,
+			'establecimiento_id' 	=> Auth::user()->establecimiento->id,
+			'details'				=> 'Bolsa Creada'
+		]);
+		Auth::user()->transitobolsa()->save($transito);
+		return $bolsa;
 	}
 
 	/**
