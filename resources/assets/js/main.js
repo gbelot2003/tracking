fb56616b3e73f889d50815789526fb494623c24a
vagrant <vagrant@homestead>
2016-04-17 04:37:40 +0000

    Revert to 564dea08a0796a79f8e1b7e97e813274e6e3a41b

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -1,15 +1,10 @@
 var $ = require('jquery');
 var app = require('angular');
-
-var ngMaterial = require('angular-material');
 var ngRoute = require('angular-route');
-var ngAria = require('angular-aria');
-var ngAnimate = require('angular-animate');
 var ngResource = require('angular-resource');
-var DataTable = require('angular-material-data-table');
 var satellizer = require('satellizer');
 
-var app = angular.module('app', [ngRoute, ngMaterial, satellizer, ngResource, DataTable]);
+var app = angular.module('app', [ngRoute, satellizer, ngResource]);
 
 require('./config')(app);
 require('./services')(app);
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,35 +2,17 @@
 <html ng-app="app">
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-    <meta http-equiv="X-UA-COMPATIBLE" content="IE=edge,chrome=1">
-    <meta name="fragment" content="!">
-    <base href="/"/>
-
-    <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no">
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="apple-mobile-web-app-status-bar-style" content="black">
-
     <title>Anguilar & webpack</title>
     <!-- change this up! http://www.bootstrapcdn.com/bootswatch/ -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,400italic">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link href="/css/angular-material.min.css" type="text/css" rel="stylesheet"/>
+    <link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/cosmo/bootstrap.min.css" type="text/css" rel="stylesheet"/>
 </head>
 
 <body>
-
-<div layout="row">
-    <div flex="100">
-        <nav>
-            <nav-info></nav-info>
-        </nav>
-    </div>
-</div>
-<div layout="layout-gt-md">
-    <div flex="80" flex-offset="10">
-        <div class=“ng-cloak” ng-view></div>
-    </div>
+<div class="container">
+    <nav>
+        <nav-info></nav-info>
+    </nav>
+    <div ng-view></div>
 </div>
 <script src="js/main.min.js"></script>
 </body>
