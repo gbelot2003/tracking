14f07ea9b091b6e22b1f68bad8caa2e52154042c
gbelot2003 <gbelot2003@hotmail.com>
2016-04-16 19:03:24 -0600

    Adding visual svg images and implementing angular-material on desing

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -1,10 +1,14 @@
 var $ = require('jquery');
 var app = require('angular');
+
+var ngMaterial = require('angular-material');
 var ngRoute = require('angular-route');
+var ngAria = require('angular-aria');
+var ngAnimate = require('angular-animate');
 var ngResource = require('angular-resource');
 var satellizer = require('satellizer');
 
-var app = angular.module('app', [ngRoute, satellizer, ngResource]);
+var app = angular.module('app', [ngRoute, ngMaterial, satellizer, ngResource]);
 
 require('./config')(app);
 require('./services')(app);
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,9 +2,20 @@
 <html ng-app="app">
 <head>
     <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <meta http-equiv="X-UA-COMPATIBLE" content="IE=edge,chrome=1">
+    <meta name="fragment" content="!">
+    <base href="/"/>
+
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximun-scale=1, user-scalable=no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+
     <title>Anguilar & webpack</title>
     <!-- change this up! http://www.bootstrapcdn.com/bootswatch/ -->
-    <link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/cosmo/bootstrap.min.css" type="text/css" rel="stylesheet"/>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,400italic">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="/css/angular-material.min.css" type="text/css" rel="stylesheet"/>
 </head>
 
 <body>
