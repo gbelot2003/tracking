4b0fd1fb14db36ef737696cc12c557f5c3a9e622
gbelot2003 <gbelot200@hotmail.com>
2016-07-12 02:28:51 -0600

    migrations: alter tables adding index and foreig keys to tables

---
+++ database/migrations/2016_07_12_020240_add_foreing_key_to_departamentos_from_municipios_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddForeingKeyToDepartamentosFromMunicipiosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('municipios', function (Blueprint $table) {
+            $table->index('departamento_id');
+            $table->foreign('departamento_id')
+                ->references('id')
+                ->on('departamentos');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('municipios', function (Blueprint $table) {
+            $table->dropForeign('departamento_id');
+            $table->dropIndex('departamento_id');
+        });
+    }
+}
---
+++ database/migrations/2016_07_12_020716_add_foreing_key_to_municipios_from_establecimientos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddForeingKeyToMunicipiosFromEstablecimientosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('establecimientos', function (Blueprint $table) {
+            $table->index('municipio_id');
+            $table->foreign('departamento_id')
+                ->references('id')
+                ->on('municipios');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('establecimientos', function (Blueprint $table) {
+            $table->dropForeign('departamento_id');
+            $table->dropIndex('departamento_id');
+        });
+    }
+}
